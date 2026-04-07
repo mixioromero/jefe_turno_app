566e4a1c15ca658c233fb5e29dfaa9cec834ff46
mixioromero <mixio.romero.22@gmail.com>
2026-04-07 12:39:57 -0400

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,15 +1,37 @@
-const CACHE_NAME = 'jefe-turno-v1';
+const CACHE_NAME = 'jefe-turno-v3';
 const URLS = [
-  '/',
   '/manifest.webmanifest',
   '/static/styles.css'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
